78b276aefd65ee63f0dacf9b34a162465cce613d
papilo yaks <papiloyaks>
2025-07-01 20:19:03 +0100

    commit

--- food-website/src/components/HeroSection.jsx
+++ food-website/src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ function HeroSection() {
       {/* Left Side - Image and Contact Info */}
       <div className="relative  lg:w-1/2">
         <img
-          src="/public/images/Chop.png" 
+          src="/images/Chop.png" 
           alt="Delicious Food"
           className="w-full h-auto rounded-lg object-cover"
         />
